82cda9421d695d68f334015ee73ca46ed15349e4
gillchristian <gillchristiang@gmail.com>
2016-01-27 22:43:19 -0300

    added current job and made minnor changes

--- scripts/controllers/portfolioCtrl.js
+++ scripts/controllers/portfolioCtrl.js
@@ -3,19 +3,27 @@ angular.module('personalApp.controllers')
       	function($scope) {
       		$scope.projects = [
             {
+              name: '4r Soluciones',
+              description: 'Front end Developer. Aug \'16 - today.',
+              url: 'http://4rsoluciones.com/',
+              img: 'assets/img/portfolio/4rsoluciones.jpg',
+              tags: ['AngularJS', 'WordPress','php', 'html5', 'ccs3']
+            },
+            {
               name: 'Poder Judicial, Entre Rios',
               description: 'WordPress plugins and theme edition, front end development.',
               url: 'http://www.jusentrerios.gov.ar/',
               img: 'assets/img/portfolio/jusentrerios.jpg',
-              tags: ['WordPress','php', 'html5', 'ccs3']
+              tags: ['WordPress','php', 'html5', 'ccs3', 'jQuery']
             },
-			{
+			      {
               name: 'ADRA Central Asia Website',
               description: 'Creation of the website, using Wordpress template.',
               url: 'http://adra-central.asia/?lang=en',
               img: 'assets/img/portfolio/ADRAwebsite.jpg',
               tags: ['WordPress','php', 'html5', 'ccs3']
-            }];
+            },
+          ];
           
           $scope.personals = [
             {
@@ -25,7 +33,7 @@ angular.module('personalApp.controllers')
               img: 'assets/img/portfolio/cpt.jpg',
               tags: ['WordPress','php']
             },
-			{
+			      {
               name: 'Army Calculator',
               description: 'Troops amounts and costs calculator for Clash of Clans.',
               url: 'http://gillchristian.github.io/clash/',
