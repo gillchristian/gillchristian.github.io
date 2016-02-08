6c7fcc1bbfc8916bbf2fb80b8834130e56be09e1
gillchristian <gillchristiang@gmail.com>
2016-02-07 23:26:23 -0300

    file permissions

--- scripts/controllers/portfolioCtrl.js
+++ scripts/controllers/portfolioCtrl.js
@@ -1,45 +1,59 @@
 angular.module('personalApp.controllers')
-   .controller('PortfolioController', ['$scope',
-      	function($scope) {
-      		$scope.projects = [
-            {
-              name: 'Front-end Developer',
-              description: 'Currently working at 4r Soluciones. Aug \'15 - today.',
-              url: 'http://4rsoluciones.com/',
-              img: 'assets/img/portfolio/4rsoluciones.jpg',
-              tags: ['AngularJS', 'WordPress','php', 'html5', 'ccs3']
-            },
-            {
-              name: 'Poder Judicial, Entre Rios',
-              description: 'WordPress plugins and theme edition, front end development.',
-              url: 'http://www.jusentrerios.gov.ar/',
-              img: 'assets/img/portfolio/jusentrerios.jpg',
-              tags: ['WordPress','php', 'html5', 'ccs3', 'jQuery']
-            },
-			      {
-              name: 'ADRA Central Asia Website',
-              description: 'Creation of the website, using Wordpress template.',
-              url: 'http://adra-central.asia/?lang=en',
-              img: 'assets/img/portfolio/ADRAwebsite.jpg',
-              tags: ['WordPress','php', 'html5', 'ccs3']
-            },
-          ];
-          
-          $scope.personals = [
-            {
-              name: 'CPT Class',
-              description: 'Helper class to create Custom Pots Types in WordPress.',
-              url: 'https://github.com/gillchristian/CPT',
-              img: 'assets/img/portfolio/cpt.jpg',
-              tags: ['WordPress','php']
-            },
-			      {
-              name: 'Army Calculator',
-              description: 'Troops amounts and costs calculator for Clash of Clans.',
-              url: 'http://gillchristian.github.io/clash/',
-              img: 'assets/img/portfolio/clash_app.jpg',
-              tags: ['angularJS','bootstrap']
-            }];
-
-    	}
-   ]);
+  .controller('PortfolioController', ['$scope',
+    function($scope) {
+      $scope.projects = [
+        {
+          name: 'Front-end Developer',
+          description: 'Currently working at 4r Soluciones. Aug \'15 - today.',
+          url: 'http://4rsoluciones.com/',
+          img: 'assets/img/portfolio/4rsoluciones.jpg',
+          tags: ['AngularJS', 'WordPress','php', 'html5', 'ccs3']
+        },
+        {
+          name: 'Poder Judicial, Entre Rios',
+          description: 'WordPress plugins and theme edition, front end development.',
+          url: 'http://www.jusentrerios.gov.ar/',
+          img: 'assets/img/portfolio/jusentrerios.jpg',
+          tags: ['WordPress','php', 'html5', 'ccs3', 'jQuery']
+        },
+        {
+          name: 'ADRA Central Asia Website',
+          description: 'Creation of the website, using Wordpress template.',
+          url: 'http://adra-central.asia/?lang=en',
+          img: 'assets/img/portfolio/ADRAwebsite.jpg',
+          tags: ['WordPress','php', 'html5', 'ccs3']
+        },
+      ];
+      
+      $scope.personals = [
+        {
+          name: 'jcorddion',
+          description: 'jQuery acorddion plugin.',
+          url: 'http://gillchristian.github.io/jcorddion/',
+          img: 'assets/img/portfolio/jcorddion.jpg',
+          tags: ['jQuery','plugin', 'accordion', 'bower-package']
+        },
+        {
+          name: 'Tii',
+          description: 'Simple JavaScript assertion library.',
+          url: 'http://npmjs.org/package/tii/',
+          img: 'assets/img/portfolio/tii.jpg',
+          tags: ['npm-package','tests', 'library', 'ES6']
+        },
+        {
+          name: 'CPT Class',
+          description: 'Helper class to create Custom Pots Types in WordPress.',
+          url: 'https://github.com/gillchristian/CPT',
+          img: 'assets/img/portfolio/cpt.jpg',
+          tags: ['WordPress','php']
+        },
+        {
+          name: 'Army Calculator',
+          description: 'Troops amounts and costs calculator for Clash of Clans.',
+          url: 'http://gillchristian.github.io/clash/',
+          img: 'assets/img/portfolio/clash_app.jpg',
+          tags: ['angularJS','bootstrap']
+        },
+      ];
+    }
+]);
