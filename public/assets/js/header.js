dd3cca559a3c65bca5380d4f6449211e32ce8c5d
gillchristian <gillchristiang@gmail.com>
2016-08-18 23:25:36 -0300

    Add offset and tolerance headroom config. :sunglasses:

--- public/assets/js/header.js
+++ public/assets/js/header.js
@@ -1,9 +1,17 @@
 var header = document.querySelector("header")
 var headerFix = document.querySelector(".header-fix")
 
-if (headerFix) {
+if (headerFix && header) {
   headerFix.style.height = header.offsetHeight + 'px'
+  
+  var config = {
+    offset : 100,
+    tolerance : {
+      up : 15,
+      down : 15
+    },
+  }
 
-  var headroom  = new Headroom(header)
+  var headroom  = new Headroom(header, config)
   headroom.init();
 }
