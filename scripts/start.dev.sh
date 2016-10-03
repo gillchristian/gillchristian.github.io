cbac6159e904922e70d0bb48f10ba67c138e6896
gillchristian <gillchristiang@gmail.com>
2016-10-02 21:22:55 -0300

    Update scripts

---
+++ scripts/start.dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NODE_ENV=development;
+
+npm run move:assets;
+
+sudo docker-compose up;
---
+++ scripts/start.prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NODE_ENV=production;
+
+npm run move:assets;
+
+npm run js:build;
+
+sudo docker-compose -f docker-compose.prod.yml up;
