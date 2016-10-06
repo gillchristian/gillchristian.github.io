e145ef3c596c09c1399461ed41839b45579cdaf2
gillchristian <gillchristiang@gmail.com>
2016-10-06 08:32:25 -0300

    Move header fix to the footer (from bundle) to improve its loading time.

--- src/enhacers/header.js
+++ src/enhacers/header.js
@@ -4,8 +4,6 @@ const header = document.querySelector('header')
 const headerFix = document.querySelector('.header-fix')
 
 if (headerFix && header) {
-  headerFix.style.height = header.offsetHeight + 'px'
-
   const headroom = new Headroom(header, {
     offset: 100,
     tolerance: {
